a154b7756b0ac972738ad0c6004284f697c1f534
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2023-10-13 22:19:11 -0400

    Update imagefinder.py

--- imagefinder.py
+++ imagefinder.py
@@ -4,16 +4,12 @@ api_key = 'AIzaSyBrWFYAQsLica1GXPmxUudAsODPCupiHZ8'
 search_engine_id = 'f6df8f5a4eb1c406c'
 query = 'your search query'
 
-# Define the API endpoint URL
 url = f'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_engine_id}&q={query}&searchType=image'
 
-# Make the API request
 response = requests.get(url)
 
-# Check if the request was successful
 if response.status_code == 200:
     data = response.json()
-    # Extract and process the image search results from the 'items' field in the JSON response.
     for item in data.get('items', []):
         image_link = item.get('link')
         print(image_link)
