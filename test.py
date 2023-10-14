a08021fe02087397312408ea3d1512ff73f439a7
TOMASCASS <98792255+TOMASCASS@users.noreply.github.com>
2023-10-14 14:59:10 -0400

    Connects previous stories to current

--- test.py
+++ test.py
@@ -2,10 +2,11 @@ import openai
 
 
 # Set your OpenAI API key
-openai.api_key = "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"
+openai.api_key = "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"
 
 stories = []
 input_text = ""
+count = 0
 
 while True:
     input_text = input("Input story idea or q to quit: \n")
@@ -13,12 +14,22 @@ while True:
     if input_text.lower() == "q":
         break
 
-    # Use the OpenAI API to summarize the text
-    response = openai.Completion.create(
-        engine="text-davinci-002",
-        prompt="Make a story from this text:\n" + input_text,
-        max_tokens=250,  # Adjust the number of tokens for your desired summary length
-    )
+    if count == 0:
+        # Use the OpenAI API to summarize the text
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt="Make a story from this text:\n" + input_text,
+            max_tokens=250,  # Adjust the number of tokens for your desired summary length
+        )
+    else:
+        response = openai.Completion.create(
+            engine="text-davinci-002",
+            prompt="Make a story by using the previous story: "
+            + stories[count - 1][0]
+            + " and this text as a continuation:\n"
+            + input_text,
+            max_tokens=250,  # Adjust the number of tokens for your desired summary length
+        )
 
     # Extract and print the generated summary
     summary = response.choices[0].text
@@ -31,6 +42,8 @@ while True:
 
     # Append image url to array
     stories.append([summary, image_url])
+
+    count += 1
 
 # Now you can use 'image_url' to view or download the generated image
 
