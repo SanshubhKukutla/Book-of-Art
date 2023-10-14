2a51e3937ad2b48fa7077ca7c0269a83b6d11fe6
TaipeiGuru <78506727+TaipeiGuru@users.noreply.github.com>
2023-10-13 22:18:52 -0400

    Update imagefinder.py

--- imagefinder.py
+++ imagefinder.py
@@ -1,12 +1,22 @@
 import requests
 
-api_key = "AIzaSyBrWFYAQsLica1GXPmxUudAsODPCupiHZ8"
-cse_id = "f6df8f5a4eb1c406c"
-query = "Your search query"
+api_key = 'AIzaSyBrWFYAQsLica1GXPmxUudAsODPCupiHZ8'
+search_engine_id = 'f6df8f5a4eb1c406c'
+query = 'your search query'
 
-url = f"https://www.googleapis.com/customsearch/v1?key={api_key}&cx={cse_id}&q={query}&searchType=image"
+# Define the API endpoint URL
+url = f'https://www.googleapis.com/customsearch/v1?key={api_key}&cx={search_engine_id}&q={query}&searchType=image'
 
+# Make the API request
 response = requests.get(url)
-data = response.json()
 
-# Extract and process the image results from the 'items' field in the 'data' JSON response.
+# Check if the request was successful
+if response.status_code == 200:
+    data = response.json()
+    # Extract and process the image search results from the 'items' field in the JSON response.
+    for item in data.get('items', []):
+        image_link = item.get('link')
+        print(image_link)
+else:
+    print(f"Request failed with status code {response.status_code}")
+
