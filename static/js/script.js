f2cbb76179395beca99d7dc9224d1eafc4856b61
Sanshubh Kukutla <kukutla.sanshubh@gmail.com>
2023-10-14 23:21:38 -0400

    working body of style, script, and index of front end

--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,2 @@
 console.log('WORKS WORKS WORKS');
 
-document.addEventListener("DOMContentLoaded", function () {
-    // Get a reference to the button element by its id
-    const clickButton = document.getElementById("clickButton");
-
-    // Add a click event listener to the button
-    clickButton.addEventListener("click", () => {
-        console.log("button worked?");
-    });
-});
-
-
