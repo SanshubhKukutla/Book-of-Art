97774249c343fd1dc2ac72e65ee03a6bdfad6aaa
Sanshubh Kukutla <kukutla.sanshubh@gmail.com>
2023-10-15 00:09:54 -0400

    book img on page 2 redirects to home

--- static/js/script.js
+++ static/js/script.js
@@ -1,2 +1,9 @@
 console.log('WORKS WORKS WORKS');
 
+  // Add a click event listener to the "frame-1" image
+  const frame1Image = document.querySelector(".frame-1");
+
+  frame1Image.addEventListener("click", function() {
+    // Redirect to the same URL as the "Create Your Story" button
+    window.location.href = "/generate_image";
+  });
