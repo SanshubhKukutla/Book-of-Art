244d40e0650dd23ad7f9f289a81eeecb8c0dcc0b
TOMASCASS <98792255+TOMASCASS@users.noreply.github.com>
2023-10-14 21:34:25 -0400

    Joined app.py and test.py

--- app.py
+++ app.py
@@ -1,4 +1,7 @@
-from flask import Flask, render_template
+from flask import Flask, request, render_template
+import openai
+
+openai.api_key = "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"
 
 app = Flask(__name__)
 
@@ -7,5 +10,42 @@ def hello_world():
     message = "Hello, Flask! This message is displayed in the browser."
     return render_template('index.html', message=message)
 
-if __name__ == '__main__':
-    app.run()
+@app.route("/generate_image", methods=["GET", "POST"])
+def generate_image():
+    img_urls = []
+
+    if request.method == "POST":
+        # Get user input from the form
+        user_texts = request.form.getlist("user_text[]")
+        print(user_texts)
+        for text in user_texts:
+            image_url = generate_image_with_text(text)
+            img_urls.append(image_url)
+        print(img_urls)
+        return render_template("storyscreen.html", image_urls=img_urls)
+
+    return render_template("storyinput.html")
+
+
+def generate_image_with_text(text, num_images=3):
+    response = openai.Completion.create(
+        engine="text-davinci-002",
+        prompt="Make a story from this text:\n" + text,
+        max_tokens=250,  # Adjust the number of tokens for your desired summary length
+    )
+
+    story = response.choices[0].text
+    print(story)
+    image_urls = []
+
+    # Generate images for the story
+    for _ in range(num_images):
+        image_response = openai.Image.create(prompt=story, n=1, size="256x256")
+        image_urls.append(image_response["data"][0]["url"])
+
+    return image_urls
+
+
+if __name__ == "__main__":
+    app.run(debug=True, port=8080)
+
