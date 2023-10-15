1eb3a80a64b690af96ef9e9a5be0b6f693c774d7
Sanshubh Kukutla <kukutla.sanshubh@gmail.com>
2023-10-14 21:25:30 -0400

    Fixed script js for button testing in JS implementation

--- static/js/script.js
+++ static/js/script.js
@@ -1,26 +1,13 @@
 console.log('WORKS WORKS WORKS');
 
 document.addEventListener("DOMContentLoaded", function () {
-    // Get a reference to the <img> element by its id
-    const clickableImage = document.getElementById("clickablePrintConsole");
-    console.log(clickableImage); // Check if this returns the image element
+    // Get a reference to the button element by its id
+    const clickButton = document.getElementById("clickButton");
 
-    // Add a click event listener to the image
-    clickableImage.addEventListener("click", () => {
-        console.log("Image clicked!");
+    // Add a click event listener to the button
+    clickButton.addEventListener("click", () => {
+        console.log("button worked?");
     });
-
-    try {
-        // Your code here
-    } catch (error) {
-        console.log("An error occurred:", error);
-    }
 });
 
-// Get a reference to the button element by its id
-const clickButton = document.getElementById("clickButton");
 
-// Add a click event listener to the button
-clickButton.addEventListener("click", () => {
-    console.log("Button clicked!");
-});
