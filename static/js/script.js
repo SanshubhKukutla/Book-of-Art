1d148576d6db031e82289c329622964f4c9a3a35
Sanshubh Kukutla <kukutla.sanshubh@gmail.com>
2023-10-15 00:29:54 -0400

    Styled and reformatted create button

--- static/js/script.js
+++ static/js/script.js
@@ -1,9 +1,7 @@
 console.log('WORKS WORKS WORKS');
 
-  // Add a click event listener to the "frame-1" image
-  const frame1Image = document.querySelector(".frame-1");
-
-  frame1Image.addEventListener("click", function() {
-    // Redirect to the same URL as the "Create Your Story" button
-    window.location.href = "/generate_image";
+document.getElementById("clickButton").addEventListener("click", function() {
+    window.location.href = "/generate_image"; // Redirect to the desired page
   });
+
+  
